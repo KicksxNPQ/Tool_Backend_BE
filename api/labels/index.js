d9ceb8a71d4b70d6515d13984d340d47b380d3cf
KicksxNPQ <quocnp.19@gmail.com>
2021-11-30 16:47:15 +0700

    fix query

--- api/labels/index.js
+++ api/labels/index.js
@@ -6,19 +6,45 @@ var {EnglishImage, EnglishCaption, VietnameseCaption} = require('../../models')
 
 router.get('/', async function(req,res,next) { 
     try {
-        const viecaptions = await VietnameseCaption.findOne({},null).sort({image_id:-1}).limit(1).exec();
-        let max_image_id = 0;
-        if (viecaptions !== null) 
-            max_image_id = viecaptions.image_id; 
-        const engcaptions = await EnglishCaption.find({},null).gt('image_id', max_image_id).sort({image_id: 1}).limit(5).exec();
-        if (engcaptions.length === 0) {
-            return res.status(200).json({
-                image: null,
-                captions: []
-            });
-        }
+        // const viecaptions = await VietnameseCaption.findOne({},null).sort({image_id:-1}).limit(1).exec();
+        // let max_image_id = 0;
+        // if (viecaptions !== null) 
+        //     max_image_id = viecaptions.image_id; 
+        // const engcaptions = await EnglishCaption.find({},null).gt('image_id', max_image_id).sort({image_id: 1}).limit(5).exec();
+        // if (engcaptions.length === 0) {
+        //     return res.status(200).json({
+        //         image: null,
+        //         captions: []
+        //     });
+        // }
+        // console.log(engimages);
+        const engcaptions = await EnglishCaption.aggregate([
+            {
+                "$lookup": {
+                    from: 'vietnamesecaptions',
+                    localField: 'image_id',
+                    foreignField: "image_id",
+                    as: 'tt',
+                },
+            },
+            // {"$unwind": "$tt"},
+            {"$sort": {"image_id": 1}},
+            {
+                $project: 
+                    {image_id:1, caption:1, tt:1, 
+                     size_of_tt: {$size: "$tt"}
+                }
+            },
+            {
+                "$match": {
+                    "size_of_tt": {
+                        $lt: 1
+                    }
+                }
+            },
+            {"$limit": 5}
+        ]);
         const engimages = await EnglishImage.findOne({}, null).where('image_id', engcaptions[0].image_id).exec();
-        console.log(engimages);
         return res.status(200).json({
             image: engimages,
             captions: engcaptions
